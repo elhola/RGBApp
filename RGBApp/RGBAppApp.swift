2e145a668fd8d0e8e8454036cca9c6ff228940e3
Yaroslav G <yarmcfly@gmail.com>
2023-06-07 12:59:28 +0300

    Initial Commit

---
+++ RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by Yaroslav G on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
